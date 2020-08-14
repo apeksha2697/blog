683a860274660176b18280afa59ced5e8e981ebc
Apeksha Singh <apekshasingh664@gmail.com>
2020-08-14 15:26:39 +0530

    added validation tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,29 +2,29 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'validation tests' do
+    let(:user) {build(:user)}
     it 'ensures first name presence' do
-      user = User.new(last_name: 'last', email: 'sample@example.com').save
-      expect(user).to eq(false)
+      user.first_name = nil
+      expect(user.save).to eq(false)
     end
 
     it 'ensures last name presence' do
-      user = User.new(first_name: 'first', email: 'sample@example.com').save
-      expect(user).to eq(false)
+      user.last_name = nil
+      expect(user.save).to eq(false)
     end
 
     it 'ensures email presence' do
-      user = User.new(last_name: 'last', first_name: 'first', password: '123456').save
-      expect(user).to eq(false)
+      user.email = nil
+      expect(user.save).to eq(false)
     end
 
     it 'ensures password presence ' do
-      user = User.new(first_name: 'first', last_name: 'last', email: 'sample@example.com').save
-      expect(user).to eq(false)
+      user.password = nil
+      expect(user.save).to eq(false)
     end
 
     it 'should save successfully ' do
-      user = User.new(first_name: 'first', last_name: 'last', email: 'sample@example.com', password: '123456').save
-      expect(user).to eq(true)
+      expect(user.save).to eq(true)
     end
   end
 
